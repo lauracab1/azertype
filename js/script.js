44ad0768e67eda07aabd7ca5114b41cb4ae32c28
laura cabos <lauracabos@gmail.com>
2024-07-22 15:29:06 +0200

    Appel API, calcul temps de frappe et reset

--- js/script.js
+++ js/script.js
@@ -1,24 +1,58 @@
 document.addEventListener("DOMContentLoaded", function() {
-  console.log("Hello World");
 
-  const wordsList = ["apple", "banana", "cherry", "date", "elderberry"];
+  let wordToGuess = "";
+  const word = document.querySelector(".word");
+  const start = document.getElementById("start");
   const userWord = document.getElementById("response");
-  console.log(userWord);
-  const button = document.getElementById("submit");
-  console.log(button);
+  const message = document.getElementById("message");
   const score = document.getElementById("score");
   let counter = 0;
+  let startTime = null;
+  let endTime = null;
+  const timer = document.getElementById("timer");
+  const restart = document.getElementById("restart");
 
-
-  button.addEventListener("click", function(event) {
+  start.addEventListener("click", function(event) {
     event.preventDefault();
-    for (let i = 0; i < wordsList.length; i++) {
-      if (userWord.value === wordsList[i]) {
-        console.log("You got it right!");
-        counter += 1;
-        break;
-      }
-    }
-    score.innerHTML = counter;
+    fetch("https://trouve-mot.fr/api/random")
+    .then((response) => response.json())
+    .then((data) => {
+      word.innerHTML = `<p>${data[0].name}</p>`;
+      wordToGuess = data[0].name;
+    });
   });
+
+  userWord.addEventListener('focus', function() {
+    startTime = new Date();
+  });
+
+  userWord.addEventListener('keydown', function(event) {
+    if (event.key === 'Enter') {
+      endTime = new Date();
+      submit(event);
+    }
+  });
+
+  function submit(event) {
+    event.preventDefault();
+    if (userWord.value === wordToGuess) {
+      message.innerText = "You got it right!";
+      let time = (endTime - startTime) / 1000;
+      timer.innerText = `Ton temps est de ${time} secondes`;
+      counter += 1;
+    } else {
+      message.innerText = "You got it wrong! Try again";
+    }
+    score.innerText = counter;
+    restart.classList.remove("d-none");
+  }
+
+  restart.addEventListener('click', function() {
+    startTime = null;
+    endTime = null;
+    restart.classList.add("d-none");
+    timer.innerText = "";
+    message.innerText = "";
+  });
+
 });
